48516b5e6d2b23fedddbf2c364a581584f88d124
DESKTOP-S25CO0A\User <VTB4049842@corp.dev.vtb>
2022-11-11 13:10:38 +0300

    change first example of instantiating

--- src/main/java/org/zhuravlev/LoaderApplication.java
+++ src/main/java/org/zhuravlev/LoaderApplication.java
@@ -16,16 +16,16 @@ public class LoaderApplication {
         CustomClassLoader1 customClassLoader1 = new CustomClassLoader1();
         CustomClassLoader2 customClassLoader2 = new CustomClassLoader2();
         try {
-            Object obj1;
-            obj1 = customClassLoader1.findClass("org.zhuravlev.loading.MyClass1").newInstance();
-            Object myClass1AndreyObject = obj1.getClass().newInstance();
-            Object myClass1AndreyObject1 = obj1.getClass().getConstructor(String.class).newInstance("Andrey1");
+            Class obj1;
+            obj1 = customClassLoader1.findClass("org.zhuravlev.loading.MyClass1");
+            Object myClass1AndreyObject = obj1.newInstance();
+            Object myClass1AndreyObject1 = obj1.getConstructor(String.class).newInstance("Andrey1");
             ((MyInterface) myClass1AndreyObject1).print();
 
             Object obj2;
             obj2 = customClassLoader2.findClass("org.zhuravlev.loading.MyClass1").newInstance();
             Object myClass1KirillObject = obj2.getClass().newInstance();
-            Object myClass1KirillObject1 = obj1.getClass().getConstructor(String.class).newInstance("Kirill1");
+            Object myClass1KirillObject1 = obj2.getClass().getConstructor(String.class).newInstance("Kirill1");
             ((MyInterface) myClass1KirillObject1).print();
 
         } catch (ClassFormatError e) {
